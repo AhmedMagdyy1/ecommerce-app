2fb34c220afe669f16480a20c4de3fd6b4fe9d51
Ahmed Magdy <ahmed10magdy20@gmail.com>
2023-11-13 00:36:05 +0200

    first-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CartComponent } from './cart/cart.component';
+import { BrandComponent } from './brand/brand.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductsComponent } from './products/products.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignOutComponent } from './sign-out/sign-out.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:"home",pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'cart',component:CartComponent},
+  {path:'brands',component:BrandComponent},
+  {path:'products',component:ProductsComponent},
+  {path:'categories',component:CategoriesComponent},
+  {path:'login',component:SignInComponent},
+  {path:'logout',component:SignOutComponent},
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
