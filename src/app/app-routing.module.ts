9ff48ab0e3416375be287d1da33cc76544610c60
AhmedMagdyy1 <ahmed10magdy20@gmail.com>
2024-08-24 17:22:08 +0300

    (Remove) Setting page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,26 +15,37 @@ import { CheckoutComponent } from './checkout/checkout.component';
 import { OrdersComponent } from './orders/orders.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:"home",pathMatch:'full'},
-  {path:'home',canActivate:[authGuard],component:HomeComponent},
-  {path:'about',canActivate:[authGuard],component:AboutComponent},
-  {path:'brands',canActivate:[authGuard],component:BrandComponent},
-  {path:'products',canActivate:[authGuard],component:ProductsComponent},
-  {path:'categories',canActivate:[authGuard],component:CategoriesComponent},
-  {path:'productDetails/:id',canActivate:[authGuard],component:ProductDetailsComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', canActivate: [authGuard], component: HomeComponent },
+  { path: 'about', canActivate: [authGuard], component: AboutComponent },
+  { path: 'brands', canActivate: [authGuard], component: BrandComponent },
+  { path: 'products', canActivate: [authGuard], component: ProductsComponent },
+  {
+    path: 'categories',
+    canActivate: [authGuard],
+    component: CategoriesComponent,
+  },
+  {
+    path: 'productDetails/:id',
+    canActivate: [authGuard],
+    component: ProductDetailsComponent,
+  },
   // lazy loading setting module
-  {path:'setting',canActivate:[authGuard],loadChildren:()=>import('../app/setting/setting.module').then((m)=>m.SettingModule)},
-  {path:'login',component:SignInComponent},
-  {path:'signup',component:SignOutComponent},
-  {path:'checkout/:cartId',component:CheckoutComponent},
-  {path:'allorders',component:OrdersComponent},
-  { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) },
-  {path:'**',component:NotFoundComponent}
+  // {path:'setting',canActivate:[authGuard],loadChildren:()=>import('../app/setting/setting.module').then((m)=>m.SettingModule)},
+  { path: 'login', component: SignInComponent },
+  { path: 'signup', component: SignOutComponent },
+  { path: 'checkout/:cartId', component: CheckoutComponent },
+  { path: 'allorders', component: OrdersComponent },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then((m) => m.CartModule),
+  },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   // to make # in the url when the app is on real server /#/cart
-  imports: [RouterModule.forRoot(routes,{useHash:true})],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
