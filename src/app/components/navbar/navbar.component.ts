9fa07cc32b482cbf7332bf7043fe0fa1ae4aafb6
AhmedMagdyy1 <ahmed10magdy20@gmail.com>
2024-10-05 23:48:27 -0500

    (Handle) navbar active link

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,43 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
 import { CartService } from '../../core/shared/cart.service';
+import { WishListService } from 'src/app/core/services/wish-list.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  numberOfItems: number = 0;
+  wishListItems: number = 0;
 
-  isLoggedIn : boolean = false
-  numberOfItems:number = 0
 
-
-  // will case error becz the navbar on initial will make the userdata value and not make it change 
+  // will case error becz the navbar on initial will make the userdata value and not make it change
   // so we need behaviorSubject
-    constructor(private _authService:AuthService,private _cartService:CartService){
-      this._authService.userData.subscribe((res)=>{
-        if(this._authService.userData.getValue()){
-          this.isLoggedIn = true
-        }else {
-          this.isLoggedIn = false
-        }
-        
-      })
+  constructor(
+    private _authService: AuthService,
+    private _cartService: CartService,
+    private _WishlistService:WishListService
+  ) {
+    this._authService.userData.subscribe((res) => {
+      if (this._authService.userData.getValue()) {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      }
+    });
 
-      this._cartService.numberOfItems.subscribe((res)=>{
-        this.numberOfItems = res
-      })
+    this._cartService.numberOfItems.subscribe((res) => {
+      this.numberOfItems = res;
+    });
     //   if (this._authService.userData != null){
     //      this.isLoggedIn = true
     //   }else {
     //     this.isLoggedIn = false
     //   }
     // }
-    }
-  
+  }
 
-    logOut(){
-      this._authService.removeToken()
-    }
+  ngOnInit(): void {
+    this._WishlistService.numberOfWishItems.subscribe((value) => {
+      this.wishListItems = value;
+    });
+  }
+
+  logOut() {
+    this._authService.removeToken();
+  }
 }
