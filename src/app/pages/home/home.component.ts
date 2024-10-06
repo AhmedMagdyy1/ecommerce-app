6b7b8a99ce8479c13622087f38bdfa616cb94ea1
AhmedMagdyy1 <ahmed10magdy20@gmail.com>
2024-10-05 23:51:14 -0500

    (Handle) Home Page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { CategoriesService } from 'src/app/core/services/categories.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  categoriesList: any[] = [];
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    margin:20,
+    navSpeed: 700,
+    autoplay: true,
+    autoplaySpeed: 500,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 5,
+      },
+    },
+    nav: true,
+  };
 
+  constructor(private _CategoriesService: CategoriesService) {}
+
+  ngOnInit(): void {
+    this._CategoriesService.getAllCategories().subscribe({
+      next: (response) => {
+        this.categoriesList = response.data;
+      },
+      error: (err) => {},
+    });
+  }
 }
