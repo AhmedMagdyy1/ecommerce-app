200d82f2a576358085e4819df82c9e34fed2d38c
AhmedMagdyy1 <ahmed10magdy20@gmail.com>
2024-10-05 23:47:57 -0500

    (Handle) product design

--- src/app/core/interfaces/product.ts
+++ src/app/core/interfaces/product.ts
@@ -1,15 +1,48 @@
 export interface Product {
-    imageCover:string,
-    price:number,
-    title:string,
-    category:Category,
-    ratingsAverage:number,
-    id:string,
-    description?:string,
-    images?:string[]
+  sold: number;
+  images: string[];
+  subcategory: Brand[];
+  ratingsQuantity: number;
+  _id: string;
+  title: string;
+  slug: string;
+  description: string;
+  quantity: number;
+  price: number;
+  imageCover: string;
+  category: Brand;
+  brand: Brand;
+  ratingsAverage: number;
+  createdAt: Date;
+  updatedAt: Date;
+  id: string;
+  priceAfterDiscount?: number;
+  availableColors?: any[];
+  isCartLoading: boolean;
+  isWishLoading: boolean;
 }
 
-
 interface Category {
-    name:string
+  name: string;
+}
+
+export interface ProductsArray {
+  results: number;
+  metadata: Metadata;
+  data: Product[];
+}
+
+export interface Brand {
+  _id: string;
+  name: string;
+  slug: string;
+  image?: string;
+  category?: Category;
+}
+
+export interface Metadata {
+  currentPage: number;
+  numberOfPages: number;
+  limit: number;
+  nextPage: number;
 }
--- src/app/pages/products/product-item/product-item.component.ts
+++ src/app/pages/products/product-item/product-item.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Product } from 'src/app/core/interfaces/product';
+import { WishListService } from 'src/app/core/services/wish-list.service';
 import { CartService } from 'src/app/core/shared/cart.service';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-product-item',
@@ -8,10 +11,12 @@ import { CartService } from 'src/app/core/shared/cart.service';
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent {
+  wishListArray: string[] = [];
+
   @Input() product:Product = {} as Product
   // @Input() product:Product | undefined
 
-  constructor(private _cart:CartService){
+  constructor(private _cart:CartService,private _WishlistService:WishListService) {
 
   }
 
@@ -21,5 +26,101 @@ export class ProductItemComponent {
       console.log(res);
       this._cart.numberOfItems.next(res.numOfCartItems)
     })
+  }
+
+  updateWishListArray(product: Product | null = null) {
+    this._WishlistService.getUserWishList().subscribe({
+      next: (response) => {
+        this.wishListArray = [];
+        for (const i of response.data) {
+          this.wishListArray.push(i.id);
+        }
+        if (product) {
+          product.isWishLoading = false;
+        }
+      },
+      error: (err) => {},
+    });
+  }
+
+
+  handleWishList(product: Product, productId: string, check: boolean) {
+    if (check) {
+      this.deleteFromWishList(product, productId);
+    } else {
+      this.addToWishList(product, productId);
+    }
+  }
+
+  deleteFromWishList(product: Product, productId: string) {
+    product.isWishLoading = true;
+    this._WishlistService.removeFromWishList(productId).subscribe({
+      next: (response) => {
+        Swal.fire({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 2000,
+          icon: 'success',
+          customClass: {
+            timerProgressBar: 'progress-bar',
+          },
+          showClass: {
+            popup: 'animate__animated animate__fadeInRightBig',
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutRightBig',
+          },
+          html: `<div>${product.title
+            .split(' ')
+            .splice(0, 2)
+            .join(
+              ' '
+            )} removed from your wishlist</div> <i class="fa-solid text-danger fa-heart-crack"></i>`,
+        });
+        this.updateWishListArray(product);
+        this._WishlistService.checkWishListItems();
+      },
+      error: (err) => {
+        product.isWishLoading = false;
+      },
+    });
+  }
+
+  addToWishList(product: Product, productId: string) {
+    product.isWishLoading = true;
+    this._WishlistService.addToWishList(productId).subscribe({
+      next: (response) => {
+        Swal.fire({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timerProgressBar: true,
+          timer: 2000,
+          icon: 'success',
+          customClass: {
+            timerProgressBar: 'progress-bar',
+          },
+          showClass: {
+            popup: 'animate__animated animate__fadeInRightBig',
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutRightBig',
+          },
+          html: `<div>${product.title
+            .split(' ')
+            .splice(0, 2)
+            .join(
+              ' '
+            )} added to your wishlist</div> <i class="fa-solid text-danger fa-heart"></i>`,
+        });
+        this.updateWishListArray(product);
+        this._WishlistService.checkWishListItems();
+      },
+      error: (err) => {
+        product.isWishLoading = false;
+      },
+    });
   }
 }
