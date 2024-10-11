8cd6619a28659d33a1db3ee035a236563a775872
AhmedMagdyy1 <ahmed10magdy20@gmail.com>
2024-10-10 20:40:23 -0500

    delete files

--- src/app/core/pipes/string-trim.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StringTrimPipe } from './string-trim.pipe';
-
-describe('StringTrimPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StringTrimPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
